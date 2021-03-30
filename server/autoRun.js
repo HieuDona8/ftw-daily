cdd2383bafceee195c94a292cd756dd0e2b4bc9c
Hồ Trung Hiếu <19424003@student.hcmus.edu.vn>
2021-03-30 16:32:38 +0700

    add auto rollback

---
+++ server/autoRun.js
@@ -0,0 +1,21 @@
+const schedule = require('node-schedule');
+const { integrationSdk, rollbackVoucher } = require('./api-util/sdk');
+const moment = require('moment');
+
+const timeRequest = moment().subtract('hours', 2).toDate();
+const checkRollback = () => {
+    integrationSdk.events.query({
+        createdAtStart: timeRequest,
+        eventTypes: 'transaction/transitioned'
+    }).then(response => {
+        response.data.data.forEach(item => {
+            const {lastTransition, protectedData } = item.attributes.resource.attributes;
+            if(lastTransition === 'transaction/expire-payment' || lastTransition === 'transition/expire') {
+                const {redeemId} = protectedData;
+                rollbackVoucher(redeemId);
+            }
+        })
+    })
+}
+
+const jobRollbackVoucher = schedule.scheduleJob('* * */2 * * *', checkRollback);
